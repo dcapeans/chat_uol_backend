b465640f98640bd643262ad3cf5ab6c4757d3d01
Daniel Capeans <dcapeans@gmail.com>
2021-06-10 18:17:04 -0300

    added register route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,38 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const users = []
+const messages = []
+
+
+// REGISTER PARTICIPANT //
+app.post("/participants", (req, res) => {
+    res.send("OK")
+    if(validateRegister(req.body.name)){
+        res.sendStatus(400)
+    } else {
+        req.body.lastStatus = Date.now()
+        users.push(req.body)
+        const logInMessage = {from: req.body.name, to: 'Todos', text: 'entrou na sala...', type: 'status', time: req.body.lastStatus}
+        messages.push(logInMessage)
+    }
+})
+
+
+
+
+
+
+const validateRegister = (registerName) => {
+    const nameExists = users.find(user => user.name === registerName)
+    const nameEmpty = registerName === 0 ? true : false
+    if(nameExists || nameEmpty){
+        return true
+    }
+    return false
+}
+
+
 
 app.listen(4000, () => {
     console.log("Server listening at port 4000")
